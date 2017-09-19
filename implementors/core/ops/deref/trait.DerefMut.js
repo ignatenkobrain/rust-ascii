eb6dee2d46c53683c7bfaaf86d3c8c282affddf1
Travis CI User <travis@example.org>
2017-09-19 19:31:59 +0000

    Documentation for tomprogrammer/rust-ascii@d2fe1d7

---
+++ implementors/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ascii"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"ascii/struct.AsciiString.html\" title=\"struct ascii::AsciiString\">AsciiString</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
